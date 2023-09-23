152eb28066129148534ed69e82cc931e45995a08
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-09-23 05:24:51 +0200

    refactor

--- messagebus.mjs
+++ messagebus.mjs
@@ -1,12 +1,19 @@
 import { PublisherMessageQueue } from './lib/message-queue.mjs';
 import { Message, MessagePriority, MessageType } from './lib/message.mjs';
 
+const privateBag = new WeakMap();
+
 const publishMessage = new Message('991109a9-27c3-4ca9-b30e-f3f267d7dc59', 'publish', MessagePriority.Low, MessageType.Publish, null);
 const subscriptionMessage = new Message('35d501a9-29aa-4ff7-9eaa-e70880f70446', 'subscriptions', MessagePriority.Low, MessageType.Subscription, null);
 
 const publisherMessageQueue = new PublisherMessageQueue();
 
+const DefaultTimeout = 100;
+
 export class MessageBus {
+    constructor() {
+        privateBag.set(this, DefaultTimeout);
+    }
     /**
      * @param { String } name
      * @param { String } channel
@@ -44,33 +51,45 @@ export class MessageBus {
             publisherMessageQueue.enqueue(newPublishMessage);
         });
     }
-    start() {
-        setInterval(async () => {
-            const publisherMessage = publisherMessageQueue.dequeue();
-            if (publisherMessage) {
-                let results_A = null;
-                let results_B = null;
-                let results_C = null;
-                let subscriberMessage = subscriptionMessage.find(publisherMessage.name, publisherMessage.channel, MessagePriority.High);
-                results_A = await subscriberMessage.notify(publisherMessage.data);
-                subscriberMessage = subscriberMessage.find(publisherMessage.name, publisherMessage.channel, MessagePriority.Medium);
-                if (subscriberMessage) {
-                    results_B = await subscriberMessage.notify(publisherMessage.data);
-                    subscriberMessage = subscriberMessage.find(publisherMessage.name, publisherMessage.channel, MessagePriority.Low);
-                    if (subscriberMessage) {
-                        results_C = await subscriberMessage.notify(publisherMessage.data);
-                    }
-                }
-                if (results_A) {
-                    return await publisherMessage.notify(results_A);
-                }
-                if (results_B) {
-                    return await publisherMessage.notify(results_B);
-                }
-                if (results_C) {
-                    return await publisherMessage.notify(results_C);
-                }
-            }
-        }, 1000);
+    async start() {
+        await trigger.call(this);
+        const intervalMil = privateBag.get(this);
+        setTimeout(async () => {
+            await this.start();
+        }, intervalMil);
     }
 };
+
+async function trigger() {
+    const publisherMessage = publisherMessageQueue.dequeue();
+    if (publisherMessage) {
+        privateBag.set(this, 5000);
+        let results_A = null;
+        let results_B = null;
+        let results_C = null;
+        let subscriberMessage = subscriptionMessage.find(publisherMessage.name, publisherMessage.channel, MessagePriority.High);
+        results_A = await subscriberMessage.notify(publisherMessage.data);
+        subscriberMessage = subscriberMessage.find(publisherMessage.name, publisherMessage.channel, MessagePriority.Medium);
+        if (subscriberMessage) {
+            results_B = await subscriberMessage.notify(publisherMessage.data);
+            subscriberMessage = subscriberMessage.find(publisherMessage.name, publisherMessage.channel, MessagePriority.Low);
+            if (subscriberMessage) {
+                results_C = await subscriberMessage.notify(publisherMessage.data);
+            }
+        }
+        if (results_A) {
+            await publisherMessage.notify(results_A);
+        } else if (results_B) {
+            await publisherMessage.notify(results_B);
+        } else if (results_C) {
+            await publisherMessage.notify(results_C);
+        }
+        privateBag.set(this, DefaultTimeout);
+    } else {
+        let intervalMil = privateBag.get(this);
+        if (intervalMil < 5000) {
+            intervalMil = intervalMil + 100;
+        }
+        privateBag.set(this, intervalMil);
+    }
+}

337d1e09842a2a8c5d911b664a861c98e68d8dd7
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-09-23 05:04:46 +0200

    refactor

--- messagebus.mjs
+++ messagebus.mjs
@@ -1,51 +1,76 @@
-import { MessageQueue } from './lib/message-queue.mjs';
+import { PublisherMessageQueue } from './lib/message-queue.mjs';
 import { Message, MessagePriority, MessageType } from './lib/message.mjs';
 
 const publishMessage = new Message('991109a9-27c3-4ca9-b30e-f3f267d7dc59', 'publish', MessagePriority.Low, MessageType.Publish, null);
 const subscriptionMessage = new Message('35d501a9-29aa-4ff7-9eaa-e70880f70446', 'subscriptions', MessagePriority.Low, MessageType.Subscription, null);
 
+const publisherMessageQueue = new PublisherMessageQueue();
+
 export class MessageBus {
     /**
-     * @param { String } Id
+     * @param { String } name
      * @param { String } channel
      * @param { MessagePriority } priority
      * @param { Function } callback
      */
-    subscribe(Id, channel, priority, callback) {
-        const channelMessage = subscription.find(channel, 'channel');
+    subscribe(name, channel, priority, callback) {
+        let channelMessage = subscriptionMessage.find(channel, 'channel', MessagePriority.High);
         if (!channelMessage) {
-            channelMessage = new Message(channel, 'channel', MessagePriority.Low, MessageType.Subscription, null);
+            channelMessage = new Message(channel, 'channel', MessagePriority.High, MessageType.Subscription, null);
             subscriptionMessage.child = channelMessage;
         }
-        const subscriberMessage = channelMessage.find(Id, channel);
-        const newSubscriberMessage = new Message(Id, channel, priority, MessageType.Subscription, null);
+        const newSubscriberMessage = new Message(name, channel, priority, MessageType.Subscription, null);
         newSubscriberMessage.callback = callback;
+        let subscriberMessage =
+            channelMessage.find(name, channel, MessagePriority.High) ||
+            channelMessage.find(name, channel, MessagePriority.Medium) ||
+            channelMessage.find(name, channel, MessagePriority.Low);
         if (subscriberMessage) {
-            if (subscriberMessage.priority === MessagePriority.High && newSubscriberMessage.priority === MessagePriority.Low) {
-                subscriberMessage.child = newSubscriberMessage
-                newSubscriberMessage.parent = newSubscriberMessage;
-            } else if (subscriberMessage.priority === MessagePriority.Low && newSubscriberMessage.priority === MessagePriority.High) {
-                subscriberMessage.parent = newSubscriberMessage;
-                newSubscriberMessage.child = newSubscriberMessage;
-            }
+            subscriberMessage.parent.child = newSubscriberMessage;
         } else {
             channelMessage.child = newSubscriberMessage;
         }
-        MessageQueue.enqueue(newSubscriberMessage);
     }
     /**
-     * @param { String } Id
+     * @param { String } name
      * @param { String } channel
-     * @param { MessagePriority } priority
      * @param { Object } data
     */
-    publish(Id, channel, priority, data = {}) {
+    publish(name, channel, data = {}) {
         return new Promise((resolve) => {
-            const newPublishMessage = new Message(Id, channel, priority, MessageType.Publish, data);
+            const newPublishMessage = new Message(name, channel, MessagePriority.High, MessageType.Publish, data);
             newPublishMessage.callback = resolve;
             publishMessage.child = newPublishMessage;
-            MessageQueue.enqueue(newPublishMessage);
-            setTimeout(() => MessageQueue.dequeue(), 1000);
+            publisherMessageQueue.enqueue(newPublishMessage);
         });
+    }
+    start() {
+        setInterval(async () => {
+            const publisherMessage = publisherMessageQueue.dequeue();
+            if (publisherMessage) {
+                let results_A = null;
+                let results_B = null;
+                let results_C = null;
+                let subscriberMessage = subscriptionMessage.find(publisherMessage.name, publisherMessage.channel, MessagePriority.High);
+                results_A = await subscriberMessage.notify(publisherMessage.data);
+                subscriberMessage = subscriberMessage.find(publisherMessage.name, publisherMessage.channel, MessagePriority.Medium);
+                if (subscriberMessage) {
+                    results_B = await subscriberMessage.notify(publisherMessage.data);
+                    subscriberMessage = subscriberMessage.find(publisherMessage.name, publisherMessage.channel, MessagePriority.Low);
+                    if (subscriberMessage) {
+                        results_C = await subscriberMessage.notify(publisherMessage.data);
+                    }
+                }
+                if (results_A) {
+                    return await publisherMessage.notify(results_A);
+                }
+                if (results_B) {
+                    return await publisherMessage.notify(results_B);
+                }
+                if (results_C) {
+                    return await publisherMessage.notify(results_C);
+                }
+            }
+        }, 1000);
     }
 };
--- specs/queue.spec.mjs
+++ specs/queue.spec.mjs
@@ -1,12 +1,34 @@
 import { MessagePriority } from '../lib/message.mjs';
 import { MessageBus } from '../messagebus.mjs';
+
 const messageBus = new MessageBus();
-describe('', async () => {
-    messageBus.subscribe("apples", "fruit", new MessagePriority.High, (data) => {
-        return 'Hello World From Subscriber';
+messageBus.start();
+messageBus.subscribe("apples", "fruit", MessagePriority.Low, ({ message }) => {
+    expect(message).toBe('Hello From Apple Publisher');
+    return 'Hello From Apple Subscriber A';
+});
+messageBus.subscribe("apples", "fruit", MessagePriority.Medium, ({ message }) => {
+    expect(message).toBe('Hello From Apple Publisher');
+    return 'Hello From Apple Subscriber B';
+});
+messageBus.subscribe("apples", "fruit", MessagePriority.High, ({ message }) => {
+    expect(message).toBe('Hello From Apple Publisher');
+    return 'Hello From Apple Subscriber C';
+});
+messageBus.subscribe("tomato", "fruit", MessagePriority.High, ({ message }) => {
+    expect(message).toBe('Hello From Tomato Publisher');
+    return 'Hello From Tomato Subscriber';
+});
+
+describe('when publishing an apple message on the fruit channel', () => {
+    it('should notify all apple subscribers', async () => {
+        const results = await messageBus.publish("apples", "fruit", { message: 'Hello From Apple Publisher' });
+        expect(results).toBe('Hello From Apple Subscriber C');
     });
-    const response = await messageBus.publish("apples", "fruit", new MessagePriority.High, { message: 'Hello World' });
-    if (response !== 'Hello World From Subscriber') {
-        throw new Error('test failed.');
-    }
+});
+describe('when publishing a tomato message on the fruit channel', () => {
+    it('should notify all tomato subscribers', async () => {
+        const results = await messageBus.publish("tomato", "fruit", { message: 'Hello From Tomato Publisher' });
+        expect(results).toBe('Hello From Tomato Subscriber');
+    });
 });

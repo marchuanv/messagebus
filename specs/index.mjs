6b372b906c1f711a6de233f65734be05a97dda1d
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-09-26 22:50:04 +0200

    refactor

--- specs/index.mjs
+++ specs/index.mjs
@@ -14,12 +14,13 @@ pem.createCertificate({ days: 365, selfSigned: true }, async (err, keys) => {
     }
     credentials.key = keys.clientKey;
     credentials.cert = keys.certificate;
-    Communication.httpServer = https.createServer(credentials).listen(3000, () => {
+    Communication.httpServer = https.createServer(credentials);
+    Communication.httpServer.listen(3000, () => {
         console.log(`http server is running on port 3000`);
+        const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
+        const jasmine = new Jasmine({ projectBaseDir: __dirname });
+        jasmine.jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+        jasmine.addMatchingSpecFiles(['**/*.spec.mjs']);
+        jasmine.execute();
     });
-    const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
-    const jasmine = new Jasmine({ projectBaseDir: __dirname });
-    jasmine.jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
-    jasmine.addMatchingSpecFiles(['**/*.spec.mjs']);
-    jasmine.execute();
 });

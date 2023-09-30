f131e46bf09b8c5c5233d7f5a47730bdc727225d
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-09-30 19:41:42 +0200

    refactor

--- specs/index.mjs
+++ specs/index.mjs
@@ -1,26 +1,7 @@
 import Jasmine from 'jasmine';
-import https from 'node:https';
-import pem from 'pem';
 import * as url from 'url';
-import { Communication } from '../lib/communication.mjs';
-const credentials = {
-    key: null,
-    cert: null,
-    passphrase: 'test1234'
-};
-pem.createCertificate({ days: 365, selfSigned: true }, async (err, keys) => {
-    if (err) {
-        throw err
-    }
-    credentials.key = keys.clientKey;
-    credentials.cert = keys.certificate;
-    Communication.httpServer = https.createServer(credentials);
-    Communication.httpServer.listen(3000, () => {
-        console.log(`http server is running on port 3000`);
-        const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
-        const jasmine = new Jasmine({ projectBaseDir: __dirname });
-        jasmine.jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
-        jasmine.addMatchingSpecFiles(['**/*.spec.mjs']);
-        jasmine.execute();
-    });
-});
+const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
+const jasmine = new Jasmine({ projectBaseDir: __dirname });
+jasmine.jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+jasmine.addMatchingSpecFiles(['**/*.spec.mjs']);
+jasmine.execute();
--- specs/queue.spec.mjs
+++ specs/queue.spec.mjs
@@ -1,37 +1,47 @@
-import { Message } from '../lib/message.mjs';
-import { MessageType } from '../lib/messagetype.mjs';
+import { https } from 'node:https';
+import pem from 'pem';
 import { Priority } from '../lib/priority.mjs';
 import { MessageBusAdapter } from '../messagebus-adapter.mjs';
-class TestMessageBusAdapter extends MessageBusAdapter {
-    constructor(channelName, hostName, hostPort) {
-        super(channelName, hostName, hostPort, Priority.High, MessageType.Default);
-    }
-    receive(message) {
-        expect(message).toBeInstanceOf(Message);
-        message.data = { message: `Hello From ${message.channel.name} Subscriber` };
-        return message;
-    }
-}
-describe('when publishing an apple message on the fruit channel', () => {
-    it('should notify all apple subscribers', async () => {
-        const adapter = new TestMessageBusAdapter('Apple', 'localhost', 3000);
-        const expectedMsg = await adapter.send(Priority.High, { message: 'Hello From Apple Publisher' });
-        expect(JSON.stringify(expectedMsg.data)).toBe(JSON.stringify({ message: 'Hello From Apple Subscriber' }));
+describe('given an https connection', () => {
+    let httpServer;
+    beforeAll(() => {
+        const credentials = { key: null, cert: null, passphrase: 'test1234' };
+        pem.createCertificate({ days: 365, selfSigned: true }, async (err, keys) => {
+            if (err) {
+                throw err
+            }
+            credentials.key = keys.clientKey;
+            credentials.cert = keys.certificate;
+            httpServer = https.createServer(credentials);
+            httpServer.listen(3000, () => {
+                console.log(`http server is running on port 3000`);
+            });
+        });
     });
-});
-describe('when publishing a tomato message on the fruit channel', () => {
-    it('should notify all tomato subscribers', async () => {
-        const adapter = new TestMessageBusAdapter('Tomato', 'localhost', 3000);
-        const expectedMsg = await adapter.send(Priority.Medium, { message: 'Hello From Tomato Publisher' });
-        expect(JSON.stringify(expectedMsg.data)).toBe(JSON.stringify({ message: 'Hello From Tomato Subscriber' }));
+    describe('when sending an apple message on the fruit channel', () => {
+        it('should notify all apple subscribers', async () => {
+            const adapter = new MessageBusAdapter('Apple', 'localhost', 3000);
+            const expectedMsg = await adapter.send(Priority.High, { message: 'Hello From Apple Publisher' });
+            expect(JSON.stringify(expectedMsg.data)).toBe(JSON.stringify({ message: 'Hello From Apple Subscriber' }));
+        });
     });
-});
-describe('when publishing a tomato message on the fruit channel', () => {
-    it('should notify all tomato subscribers', async () => {
-        const adapter = new TestMessageBusAdapter('Apple', 'localhost', 3000);
-        let expectedMsg = await adapter.send(Priority.Medium, { message: 'Hello From Apple Publisher' });
-        expect(JSON.stringify(expectedMsg.data)).toBe(JSON.stringify({ message: 'Hello From Apple Subscriber' }));
-        expectedMsg = await adapter.send(Priority.High, { message: 'Hello From Apple Publisher' });
-        expect(JSON.stringify(expectedMsg.data)).toBe(JSON.stringify({ message: 'Hello From Apple Subscriber' }));
+    describe('when sending a tomato message on the fruit channel', () => {
+        it('should notify all tomato subscribers', async () => {
+            const adapter = new MessageBusAdapter('Tomato', 'localhost', 3000);
+            const expectedMsg = await adapter.send(Priority.Medium, { message: 'Hello From Tomato Publisher' });
+            expect(JSON.stringify(expectedMsg.data)).toBe(JSON.stringify({ message: 'Hello From Tomato Subscriber' }));
+        });
+    });
+    describe('when sending a tomato message on the fruit channel', () => {
+        it('should notify all tomato subscribers', async () => {
+            const adapter = new MessageBusAdapter('Apple', 'localhost', 3000);
+            let expectedMsg = await adapter.send(Priority.Medium, { message: 'Hello From Apple Publisher' });
+            expect(JSON.stringify(expectedMsg.data)).toBe(JSON.stringify({ message: 'Hello From Apple Subscriber' }));
+            expectedMsg = await adapter.send(Priority.High, { message: 'Hello From Apple Publisher' });
+            expect(JSON.stringify(expectedMsg.data)).toBe(JSON.stringify({ message: 'Hello From Apple Subscriber' }));
+        });
+    });
+    afterAllAll(() => {
+        httpServer.close();
     });
 });
